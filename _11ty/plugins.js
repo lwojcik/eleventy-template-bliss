cc05357562516410c6b2baa427850b4d84cb48e2
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-05-10 02:42:15 +0200

    Switch to automatic favicon and webmanifest generation

--- _11ty/plugins.js
+++ _11ty/plugins.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 const { IS_PRODUCTION } = require('./constants');
+const siteConfig = require('../content/_data/siteConfig');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
 const pluginEmoji = require('eleventy-plugin-emoji');
@@ -7,6 +8,7 @@ const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const srcSet = require('./plugins/srcset');
 const pluginPWA = require('eleventy-plugin-pwa-v2');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const faviconsPlugin = require('eleventy-plugin-gen-favicons');
 
 const productionPlugins = IS_PRODUCTION
   ? [
@@ -34,6 +36,23 @@ const plugins = [
   },
   {
     body: syntaxHighlight,
+  },
+  {
+    body: faviconsPlugin,
+    options: {
+      manifestData: {
+        name: siteConfig.site.title,
+        lang: siteConfig.site.language,
+        short_name: siteConfig.site.title,
+        description: siteConfig.site.description,
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        theme_color: siteConfig.manifestJson.themeColor,
+        background_color: siteConfig.manifestJson.backgroundColor,
+        orientation: 'any',
+      },
+    }
   },
 ];
 
